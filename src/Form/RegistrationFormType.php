a6205e58a85919946d42759813ab4bd6e0084131
Tony BLARD <tonyblard55@gmail.com>
2024-04-24 19:27:05 +0200

    Feat : Add attr for Submit button

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -49,6 +49,7 @@ class RegistrationFormType extends AbstractType
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Register',
+                'attr' => ['class' => 'btn btn-primary btn-lg']
             ])
         ;
     }
