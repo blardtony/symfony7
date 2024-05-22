b5e97990996858d193d3b4742333218e9fa54ac5
Tony BLARD <tonyblard55@gmail.com>
2024-05-22 21:15:30 +0200

    Feat : Remove bootstrap

--- importmap.php
+++ importmap.php
@@ -24,15 +24,5 @@ return [
     ],
     '@hotwired/turbo' => [
         'version' => '8.0.4',
-    ],
-    'bootstrap' => [
-        'version' => '5.3.3',
-    ],
-    '@popperjs/core' => [
-        'version' => '2.11.8',
-    ],
-    'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
-        'type' => 'css',
-    ],
+    ]
 ];
